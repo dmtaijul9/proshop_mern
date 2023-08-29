bd72c5b47494caa99d7e524c0f441b30a57aafa3
Taijul Islam <taijulislam.st9@gmail.com>
2023-08-30 02:38:35 +0600

    feat(cart): item added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
     <>
       <Router>
         <Header />
-        <main>
+        <main
+          style={{
+            padding: "1rem 0",
+          }}
+        >
           <Container>
             <Route path="/" component={HomeScreen} exact />
             <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
           </Container>
         </main>
         <Footer />
